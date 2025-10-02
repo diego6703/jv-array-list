023be77bdcb861171894f31d25386d4cb93fa81f
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-10-02 09:50:54 +0200

    Task ArrayList, test positive impl

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,133 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    public static final int DEFAULT_SIZE = 10;
+    public static final String WRONG_INDEX_MESSAGE = "that index doesn't exist";
+    private int currentSize;
+    private T[] classList;
+    private int nextFreeIndex;
+
+    @SuppressWarnings("unchecked")
+    public ArrayList() {
+        this.classList = (T[]) new Object[DEFAULT_SIZE];
+        this.nextFreeIndex = 0;
+        this.currentSize = DEFAULT_SIZE;
+    }
+
     @Override
     public void add(T value) {
-
+        if (nextFreeIndex == currentSize) {
+            grow();
+        }
+        classList[nextFreeIndex] = value;
+        nextFreeIndex++;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void add(T value, int index) {
-
+        if (index > size() || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(WRONG_INDEX_MESSAGE);
+        }
+        if (nextFreeIndex == currentSize) {
+            grow();
+        }
+        T[] newArr = (T[]) new Object[currentSize];
+        if (index > 0) {
+            System.arraycopy(classList, 0, newArr, 0, index);
+        }
+        if (index <= size()) {
+            System.arraycopy(classList, index, newArr, index + 1, size() - index);
+        }
+        newArr[index] = value;
+        classList = newArr;
+        nextFreeIndex++;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        checkIfIndexExist(index);
+        return classList[index];
     }
 
     @Override
     public void set(T value, int index) {
-
+        checkIfIndexExist(index);
+        classList[index] = value;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public T remove(int index) {
-        return null;
+        checkIfIndexExist(index);
+        T[] newArr = (T[]) new Object[currentSize];
+        if (index > 0) {
+            System.arraycopy(classList, 0, newArr, 0, index);
+        }
+        if (index < size()) {
+            System.arraycopy(classList, index + 1, newArr, index, size() - index - 1);
+        }
+        T removedObject = get(index);
+        classList = newArr;
+        nextFreeIndex--;
+        return removedObject;
+
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        boolean foundElement = false;
+        for (int i = 0; i < currentSize; i++) {
+            if (element == null && classList[i] == null) {
+                remove(i);
+                foundElement = true;
+                break;
+            }
+            if (classList[i] != null && classList[i].equals(element)) {
+                remove(i);
+                foundElement = true;
+                break;
+            }
+        }
+        if (!foundElement) {
+            throw new NoSuchElementException();
+        }
+        return element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return nextFreeIndex;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return nextFreeIndex == 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void grow() {
+        int oldSize = currentSize;
+        int newSize = (int) (oldSize * 1.5);
+        T[] newArr = (T[]) new Object[newSize];
+        System.arraycopy(classList, 0, newArr, 0, oldSize);
+        currentSize = newSize;
+        classList = newArr;
+    }
+
+    private void checkIfIndexExist(int index) {
+        if (index >= size() || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException(WRONG_INDEX_MESSAGE);
+        }
     }
 }
